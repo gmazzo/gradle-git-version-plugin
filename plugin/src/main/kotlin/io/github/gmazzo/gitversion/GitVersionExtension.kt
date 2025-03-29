8ac5a4d0360284b2194964761f372519f7db7368
Guillermo Mazzola <gmazzo65@gmail.com>
2025-03-29 21:30:16 +0100

    Improved DSL

--- plugin/src/main/kotlin/io/github/gmazzo/gitversion/GitVersionExtension.kt
+++ plugin/src/main/kotlin/io/github/gmazzo/gitversion/GitVersionExtension.kt
@@ -1,12 +1,17 @@
 package io.github.gmazzo.gitversion
 
+import org.gradle.api.Named
 import org.gradle.api.provider.Property
 
-abstract class GitVersionExtension() {
+abstract class GitVersionExtension() : Named {
 
     abstract val tagPrefix: Property<String>
 
+    abstract val forceSnapshot : Property<Boolean>
+
     abstract val initialVersion: Property<String>
+
+    abstract val versionProducer: Property<Class<out GitVersionValueSource>>
 
     abstract val version: Property<String>
 
